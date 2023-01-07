90d45df440d22028d4f2465fba742d07b02db8f3
glebbykov <122178014+glebbykov@users.noreply.github.com>
2023-01-08 01:24:02 +0300

    Update install_docker.sh
    case construct

--- install_docker.sh
+++ install_docker.sh
@@ -1,1 +1,46 @@
+#!/bin/bash
 
+# Identify the Linux platform
+platform=$(cat /etc/*-release | grep -E '^ID=' | cut -d '=' -f 2 | tr -d '"')
+
+# Identify the platform version
+version=$(cat /etc/*-release | grep -E '^VERSION_ID=' | cut -d '=' -f 2 | tr -d '"')
+
+echo "Detected platform: $platform $version"
+
+# Depending on the platform detected, the target script is launched
+case "$platform" in
+
+  CentOS)
+    ./centos.sh
+    ;;
+
+  Debian)
+    ./debian.sh
+    ;;
+
+  Fedora)
+    ./fedora.sh
+    ;;
+
+  Raspbian)
+    ./raspbian.sh
+    ;;
+
+  RHEL)
+    ./rhel.sh
+    ;;
+
+  SLES)
+    ./sles.sh
+    ;;
+    
+  Ubuntu)
+    ./ubuntu.sh
+    ;;
+  *)
+    # Еrror message
+    echo "Error: Unrecognized platform"
+    exit 1
+    ;;
+esac
