a06412f5d16affc7f0360f87719b5307070859fd
glebbykov <122178014+glebbykov@users.noreply.github.com>
2023-01-08 01:57:23 +0300

    Create centos.sh

---
+++ scripts_for_os/centos.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "Select which utility packages to install:"
+echo "1. Docker"
+echo "2. Git"
+echo "3. Python"
+echo "4. Python3"
+echo "5. Ansible"
+echo "6. Exit"
+
+read -p "Enter your selections (1-6, separated by a space): " -a selections
+
+# The case statements are nested within a loop to allow the user to make multiple selections
+for selection in "${selections[@]}"
+do
+  case "$selection" in
+    1)
+      ./docker.sh
+      ;;
+    2)
+      ./git.sh
+      ;;
+    3)
+      ./python.sh
+      ;;
+    4)
+      ./python3.sh
+      ;;
+    5)
+      ./ansible.sh
+      ;;
+    6)
+      # Exit the script
+      exit 0
+      ;;
+    *)
+      # Еrror message
+      echo "Error: Invalid selection"
+      exit 1
+      ;;
+  esac
+done
