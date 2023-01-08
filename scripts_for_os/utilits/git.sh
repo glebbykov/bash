5eb6e793e01cb3a0318edc56df0b74c7719d5611
glebbykov <122178014+glebbykov@users.noreply.github.com>
2023-01-08 20:13:29 +0300

    email
    +read -p "Enter your email: " email

--- scripts_for_os/utilits/git.sh
+++ scripts_for_os/utilits/git.sh
@@ -8,6 +8,7 @@ git --version
 
 # Prompt the user to enter their name and email
 read -p "Enter your name: " name
+read -p "Enter your email: " email
 
 # Set the user's name and email in Git
 git config --global user.name "$name"
