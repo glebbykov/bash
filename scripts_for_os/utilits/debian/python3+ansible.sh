a92abea330cb0c2d5a7eccd55d8989cdd90eae29
glebbykov <122178014+glebbykov@users.noreply.github.com>
2023-01-08 21:27:28 +0300

    Update python3+ansible.sh

--- scripts_for_os/utilits/debian/python3+ansible.sh
+++ scripts_for_os/utilits/debian/python3+ansible.sh
@@ -2,10 +2,8 @@
 
 sudo apt-get update
 
-# Install Python 3 and pip
 sudo apt-get install -y python3 python3-pip
-
-# Install the latest version of ansible
 sudo pip3 install ansible
 
 echo "Python 3 and Ansible have been successfully installed!"
+echo "Ansible version: $(ansible --version | head -n 1 | awk '{print $3}'| tr -d ']')"
