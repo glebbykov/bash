fea6b95db24374469a5e7cfc5bf21973cfd300a7
glebbykov <122178014+glebbykov@users.noreply.github.com>
2023-01-08 21:03:20 +0300

    Create python3.sh

---
+++ scripts_for_os/utilits/debian/python3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# Install Python 3 and pip
+sudo apt-get install -y python3 python3-pip
+
+python3 --version
+
+echo "Python 3 has been successfully installed!"
