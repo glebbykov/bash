d0ccf2407d570b6994ac357eb09d1b1bda7f8e69
glebbykov <122178014+glebbykov@users.noreply.github.com>
2023-01-08 20:45:57 +0300

    update path

--- scripts_for_os/centos.sh
+++ scripts_for_os/centos.sh
@@ -4,7 +4,7 @@ echo "Select which utility packages to install:"
 echo "1. Docker"
 echo "2. Git"
 echo "3. Python3"
-echo "4. Ansible"
+echo "4. Python3 + Ansible"
 echo "5. Exit"
 
 read -p "Enter your selections (1-5, separated by a space): " -a selections
@@ -14,16 +14,16 @@ for selection in "${selections[@]}"
 do
   case "$selection" in
     1)
-      utilits/docker.sh
+      utilits/centos/docker.sh
       ;;
     2)
-      utilits/git.sh
+      utilits/centos/git.sh
       ;;
     3)
-      utilits/python3.sh
+      utilits/centos/python3.sh
       ;;
     4)
-      utilits/ansible.sh
+      utilits/centos/python3+ansible.sh
       ;;
     5)
       # Exit the script
