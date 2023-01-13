d59df4fbf1f7fda55b24205d9b290f41d00562ef
glebbykov <122178014+glebbykov@users.noreply.github.com>
2023-01-13 07:26:31 +0300

    Update docker.sh

--- scripts_for_os/utilits/suse/docker.sh
+++ scripts_for_os/utilits/suse/docker.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+sudo zypper refresh
+
+sudo zypper addrepo --name "Docker Repository" https://download.docker.com/linux/sles/15/x86_64/stable/
+
+sudo zypper install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo systemctl start docker
+
+sudo systemctl enable docker
+
+sudo docker run hello-world
+
+sudo usermod -aG docker $USER
+
+echo "Please log out and log back in for the changes to take effect"
+
+docker-compose --version
+
+echo "Docker and Docker Compose have been successfully installed!"
