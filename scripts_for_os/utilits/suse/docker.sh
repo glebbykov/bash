09392bdd468f1c75c396e79ac325016f3f6e2660
glebbykov <glebbykov1993@gmail.com>
2023-01-13 09:17:30 +0300

    suse docker falled

--- scripts_for_os/utilits/suse/docker.sh
+++ scripts_for_os/utilits/suse/docker.sh
@@ -2,7 +2,23 @@
 
 sudo zypper refresh
 
-sudo zypper install docker docker-compose docker-compose-switch
+opensuse_repo="https://download.opensuse.org/repositories/security:SELinux/SLE_15_SP3/security:SELinux.repo"
+
+sudo zypper addrepo $opensuse_repo
+
+sudo zypper remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine \
+                  runc
+
+sudo zypper addrepo https://download.docker.com/linux/sles/docker-ce.repo
+
+sudo zypper install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo systemctl start docker
 
