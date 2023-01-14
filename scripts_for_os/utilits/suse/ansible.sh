5019a01620d2e01e62a913bd7c61a36d7a9868bd
glebbykov <122178014+glebbykov@users.noreply.github.com>
2023-01-14 22:59:35 +0300

    Delete scripts_for_os/utilits/suse directory

--- scripts_for_os/utilits/suse/ansible.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo zypper refresh
-
-sudo zypper install -y ansible
-
-ansible --version
-
-echo "Ansible has been successfully installed!"
--- scripts_for_os/utilits/suse/python3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo zypper refresh
-
-sudo zypper install -y python3 python3-pip
-
-python3 --version
-
-echo "Python3 has been successfully installed!"
